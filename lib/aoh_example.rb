6e7a2fcb36a2a7840223145cda1e1e0a0998973c
Jesse Langley <jlangleyiii@aol.com>
2020-04-11 19:07:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,8 +23,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-   aoh_lookup = aoh[row][key] = new_value
-  puts aoh_lookup 
+   aoh_update = aoh[row][key] = new_value
+  puts aoh_update 
   
   
   # Update the AoH data at row and key to have the value of new_value
